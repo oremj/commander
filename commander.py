d651d104d58f81619f05cc6fcd43a6e2f630b8e0
Jeremy Orem <jeremy.orem@gmail.com>
2011-02-16 19:48:52 -0800

    abstract threading

--- commander.py
+++ commander.py
@@ -1,16 +1,57 @@
+import logging
 from colorprint import colorize
-import threading
 from subprocess import Popen, PIPE
+from threading import Lock, Semaphore, Thread
 
+
+log = logging
+
+
+class ThreadPool:
+    
+    def __init__(self, max_threads):
+        self._thread_available = Semaphore(max_threads)
+        self.threads = []
+    
+    def _thread_func(self, f):
+        def func(*args, **kwargs):
+            try:
+                f(*args, **kwargs)
+            except Exception, e:
+                print e
+                pass
+
+            log.debug("Releasing semaphore")
+            self._thread_available.release()
+
+        return func
+
+    def add_func(self, f, *args, **kwargs):
+        t = Thread(target=self._thread_func(f),
+                            args=args, kwargs=kwargs)
+        t.daemon = True
+        self.threads.append(t)
+
+    def run_all(self):
+        for t in self.threads:
+            log.debug("Waiting for semaphore")
+            self._thread_available.acquire(True)
+            log.debug("Starting thread")
+            t.start()
+
+        log.debug("Waiting for threads to finish")
+        for t in self.threads:
+            t.join()
+        
 
 class Commander:
 
     def __init__(self, remote_limit=8):
-        self._thread_available = threading.Semaphore(remote_limit)
-        self._output_lock = threading.Lock()
+        self.remote_limit = remote_limit
+        self._output_lock = Lock()
 
     def remote(self, hosts, cmd, jumphost=None):
-        threads = []
+        t = ThreadPool(self.remote_limit)
         extra = []
         if jumphost:
             extra.append('-o "ProxyCommand ssh -A %s nc %%h %%p"' % jumphost)
@@ -18,28 +59,9 @@ class Commander:
             ssh_cmd = """ssh -T %s %s <<EOF
                 %s
 EOF""" % (" ".join(extra), host, cmd)
+            t.add_func(self._run_command, host, cmd, ssh_cmd)
 
-            t = threading.Thread(target=self._run_command_thread,
-                                    args=(host, cmd, ssh_cmd))
-            t.daemon = True
-            threads.append(t)
-
-        for t in threads:
-            t.start()
-
-        for t in threads:
-            t.join()
-
-    def _run_command_thread(self, *args, **kwargs):
-        self._thread_available.acquire(True)
-
-        try:
-            self._run_command(*args, **kwargs)
-        except Exception, e:
-            print e
-            pass
-
-        self._thread_available.release()
+        t.run_all()
 
     def _run_command(self, host, cmd, full_cmd=None):
         if not full_cmd:
@@ -62,4 +84,4 @@ EOF""" % (" ".join(extra), host, cmd)
 
     def _log_lines(self, host, out_type, output):
         for l in output.splitlines():
-            print '[%s] %s: %s' % (colorize(host, 'green'), out_type, l)
+            print '[%s] %s: %s' % (colorize(host, 'green'), out_type, l.strip())
