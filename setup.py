80d0e785f2d2a8b84feedf72b7401a4f8c25e72c
Jeremiah Orem <jeremy.orem@gmail.com>
2012-07-12 16:45:07 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from distutils.core import setup
 
 setup(name='commander',
-      version='0.2.2',
+      version='0.2.3',
+      author='Jeremiah Orem',
+      url='http://github.com/oremj/commander',
+      author_email='oremj@oremj.com',
+      description='A deployment library and remote command runner.',
       packages=['commander'],
       scripts=['scripts/issue-multi-command', 'scripts/commander'],
       )
