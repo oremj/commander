fb205a0d6327103ce43fdbaf2cbe3649a6dd2a1a
Jeremy Orem <jeremy.orem@gmail.com>
2013-04-18 17:09:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='commander',
-      version='0.2.4',
+      version='0.2.5',
       author='Jeremiah Orem',
       url='http://github.com/oremj/commander',
       author_email='oremj@oremj.com',
